b7755e2ad23ffdf04ff87ae22e52da7f7de411cd
JosueCaal <88689206+JosueCaal@users.noreply.github.com>
2023-06-13 14:33:14 -0600

    Add files via upload

---
+++ ciclos2.cpp
@@ -0,0 +1,30 @@
+/*
+	ciclos -- estructuras repetitivas
+	
+	para ==> for 
+	
+	for(inicio; final; incremento o decremento){
+		i=0-200		=50 		i=i+1==>i++ -- i=i-1==>i--
+	}
+	
+*/
+
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+	
+	int tabla=9;
+
+	
+	for(int i=0;i<=10;i++){
+		cout<<tabla<<" X "<<i<<" = "<<i*tabla<<endl;
+		
+	}
+	
+	
+	
+	
+}
